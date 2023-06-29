a540a6b0e0cd8730715cd09addd6b276bcc963be
Rohithreddy969 <rohithreddy969@gmail.com>
2023-06-29 16:00:36 +0530

    Foldernameupdate

---
+++ Accessmodifiers/Default_access_modifiersCalculator.java
@@ -0,0 +1,19 @@
+package demo1;
+
+public class Calculator {
+	
+//Default access modifier
+	void add(int a, int b) {
+		System.out.println(a+b);
+	}
+	
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		
+		calculator.add(6, 6);
+
+	}
+}
+
+
+
---
+++ Accessmodifiers/Test1.java
@@ -0,0 +1,10 @@
+package demo1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		
+		calculator.add(5, 5);
+	}
+}
